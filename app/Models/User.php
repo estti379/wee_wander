ac43133115e45deac00b84d3d6792ddcefa4ef95
estti379 <estti379@gmail.com>
2023-05-15 15:31:33 +0200

    migration and seeding of users

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,12 +5,10 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +16,15 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
+        'email',
+        'firstname',
+        'lastname',
+        'picture',
+        'description',
+        'car_owned',
+        'driver_license',
     ];
 
     /**
@@ -30,7 +34,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -39,6 +42,6 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+
     ];
 }
--- database/migrations/2023_05_15_121939_create_users_table.php
+++ database/migrations/2023_05_15_121939_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('picture');
+            $table->longText('description');
+            $table->enum('car_owned', ['yes', 'no', 'hidden']);
+            $table->enum('driver_license', ['yes', 'no', 'hidden']);
             $table->rememberToken();
             $table->timestamps();
         });
