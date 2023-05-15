05c61204c356521707d69766fc27482332ab9731
estti379 <estti379@gmail.com>
2023-05-15 16:57:40 +0200

    database migrations
    -routes still no done
    - not tested

---
+++ database/migrations/2023_05_15_150615_create_adventures_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('adventures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_event');
+            $table->unsignedBigInteger('id_trail');
+            $table->dateTime('start_date');
+            $table->dateTime('due_date');
+        });
+
+        Schema::table('adventures', function(Blueprint $table) {
+            $table->foreign('id_event')->references('id')->on('events');
+            $table->foreign('id_trail')->references('id')->on('trails');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('adventures');
+    }
+};
