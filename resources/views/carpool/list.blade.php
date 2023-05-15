bc0d4fddd4bcb54349ce060a19fe162ed25c0635
Modjo <ch.maurice@hotmail.fr>
2023-05-15 15:06:44 +0200

    shareroad component

---
+++ resources/views/carpool/list.blade.php
@@ -0,0 +1,9 @@
+{{-- ShareRoad List --}}
+
+
+<x-layout>
+    <h2>Available carpool</h2>
+    <ul>
+        <li><x-share_road_detail/></li> 
+    </ul>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CarPool List
+Route::get('/carpool/list',function(){
+    return view('carpool.list');
+});
