5a412f6b3c60f5678514c9e9aed97dcee599fc70
estti379 <estti379@gmail.com>
2023-05-16 10:21:52 +0200

    Database migration finished
    -no seeding yet

--- database/migrations/2023_05_15_153300_create_routes_table.php
+++ database/migrations/2023_05_15_153300_create_routes_table.php
@@ -13,8 +13,29 @@ return new class extends Migration
     {
         Schema::create('routes', function (Blueprint $table) {
             $table->id();
-
+            $table->string('start_location_long');
+            $table->string('start_location_latit');
+            $table->string('end_location_long');
+            $table->string('end_location_latit');
+            $table->float('distance');
+            $table->dateTime('start_date');
+            $table->float('price');
+            $table->integer('max_seats');
+            $table->integer('bike_capacity');
+            $table->boolean('pets_allowed');
+            $table->boolean('smokers_allowed');
+            $table->boolean('lugage');
+            $table->unsignedBigInteger('id_carowner');
+            $table->unsignedBigInteger('id_start_adventure');
+            $table->unsignedBigInteger('id_end_adventure');
         });
+
+        Schema::table('routes', function(Blueprint $table) {
+            $table->foreign('id_carowner')->references('id')->on('users');
+            $table->foreign('id_start_adventure')->references('id')->on('adventures');
+            $table->foreign('id_end_adventure')->references('id')->on('adventures');
+        });
+
     }
 
     /**
