885bdd06f5cd17f7c9bc261ef7edb2a7e7f1b76e
estti379 <estti379@gmail.com>
2023-05-17 10:21:13 +0200

    renaming "Trails" Model to "Trail"
    -to keep in line with laravel naming conventions

--- app/Models/Adventure.php
+++ app/Models/Adventure.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Trails;
+use App\Models\Trail;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -31,7 +31,7 @@ class Adventure extends Model
 
     public function trails(): BelongsTo
     {
-        return $this->belongsTo(Trails::class, 'id_trail');
+        return $this->belongsTo(Trail::class, 'id_trail');
     }
 
     public function routes_start(): HasMany
