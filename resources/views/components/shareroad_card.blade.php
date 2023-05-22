3472a9fa44bddb4058254c70fcb14fa43ed398a5
Maurice <120386127+mojo-lxb@users.noreply.github.com>
2023-05-22 22:56:17 +0200

    Front-end Usermanagement

--- resources/views/components/shareroad_card.blade.php
+++ resources/views/components/shareroad_card.blade.php
@@ -5,25 +5,33 @@
 {{--================== SHARE ROAD CARD ==========================--}}
 {{--@section ('last_carpool')--}}
 @props(['element'])
-
-<div class="shareroad_card">
+<div class="card">
+    <h5 class="card-header">This carpool drives to {{ $element->start_adventure->trail->name }} on the {{ $element->start_adventure->start_date }}</h5>
+    <div class="card-body">
     <h2>The carpool drives to {{ $element->start_adventure->trail->name }} on the {{ $element->start_adventure->start_date }}</h2>
         <p>Username: {{$element->carowner->username}}</p>
         <p>City Departure: {{ $element->start_location_long }},{{ $element->start_location_latit }}</p>  
         <p>Adventure Name: {{ $element->start_adventure->trail->name }} </p>                     
-        <p>Adventure Location: {{ $element->end_location_long }},{{ $element->end_location_latit }}</p>        
+        <p>Adventure Location: {{ $element->end_location_long }},{{ $element->end_location_latit }}</p> 
+        <p>Seats available: <strong>{{ $element->max_seats}}</strong></p>
+        <p><strong>Asked price :  {{ $element->price }}</strong></p>
+        <a href="#"class="btn btn-primary">Driver info</a><a href="/carpool/{{$element->id}}" class="btn btn-primary">Carpool Details</a>
+       
         {{--==TO TRANSFERT TO SINGLE ROAD==--}}
         {{--==============================--}}
         <p>Asked price :  {{ $element->price }}</p>
         {{--{{ route('carpool.show', ['id' => 1]) }} OR "carpool/{{$element->id}}"--}}
         <div>
+            <div>
+                <div>
+
 
 @php
     //TO DO :move logic to controller
 @endphp                 
 <!--========================= Checks if it's the creator ===============================-->
             @if (Auth::check() && Auth::user()->id == $element->carowner->id)
-                <button><a href="carpool/edit/{{$element->id}}">Edit</a>    
+            <a href="carpool/edit/{{$element->id}}" class="btn btn-primary">Edit</a>  
             @endif
                 
             <div>
@@ -32,7 +40,7 @@
                     <!-- ========================= Button to join the carpool =========================-->
                     <form action="/carpool/join/{{ $element->id }}" method="POST">
                         @csrf
-                        <button type="submit">Join Carpool</button>
+                        <button type="submit" class="btn btn-primary">Join Carpool</button>
                     </form>
                 @endif
             </div>
