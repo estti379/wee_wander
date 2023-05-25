75b6e9065579e54e3c75aec5b1797e1266a19d52
Estti379 <estti379@gmail.com>
2023-05-25 03:56:55 +0200

    user cannot join event after due date
    -user cannot join carpool after start_date

--- resources/views/components/events/join-button.blade.php
+++ resources/views/components/events/join-button.blade.php
@@ -5,20 +5,26 @@
     <!-- ========================= Button to join the adventure =========================-->
     @php
         $alreadyJoined = $adventure->participants->where("id", Auth::user()->id)->count() > 0;
-                
+        $isTooLate = new DateTime($adventure->due_date) < new DateTime("now");
     @endphp
-    @if (!$alreadyJoined)
-        <form action="/adventure/join/{{ $adventure->id }}" method="POST">
-        @csrf
-            <button type="submit" class="btn btn-primary">Join Adventure</button>
-        </form>
+    @if ($isTooLate)
+    <span>It is too late to join this Adventure</span>
+    <br>
+    <br>
     @else
-        @if ($alreadyJoined)
-            <span>You are already part of this Adventure</span>
-            <form action="/adventure/withdraw/{{ $adventure->id }}" method="POST">
-                @csrf
-                    <button type="submit" class="btn btn-primary">Withdraw from Adventure</button>
-                </form>
+        @if (!$alreadyJoined)
+            <form action="/adventure/join/{{ $adventure->id }}" method="POST">
+            @csrf
+                <button type="submit" class="btn btn-primary">Join Adventure</button>
+            </form>
+        @else
+            @if ($alreadyJoined)
+                <span>You are already part of this Adventure</span>
+                <form action="/adventure/withdraw/{{ $adventure->id }}" method="POST">
+                    @csrf
+                        <button type="submit" class="btn btn-primary">Withdraw from Adventure</button>
+                    </form>
+            @endif
         @endif
     @endif
 
