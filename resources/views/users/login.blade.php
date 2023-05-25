895f46339148e81f646c242d7c5992515496a418
estti379 <estti379@gmail.com>
2023-05-25 10:01:56 +0200

    create blade no more dd()
    -fixed double aler on login

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -1,9 +1,4 @@
 <x-layout :pageTitle="$pageTitle">
-@if (session('message'))
-    <div class="alert">
-        {{ session('message') }}
-    </div>
-@endif
 <div class="card">
     <h5 class="card-header">Login</h5>
     <div class="card-body">
