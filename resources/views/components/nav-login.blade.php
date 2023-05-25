ec596ac1f68bae1c31f7c276e4ce40652beb9416
Maurice <120386127+mojo-lxb@users.noreply.github.com>
2023-05-25 08:01:13 +0200

    update

--- resources/views/components/nav-login.blade.php
+++ resources/views/components/nav-login.blade.php
@@ -5,10 +5,10 @@
     @endphp
     <span>Hello {{$sessionUser->firstname}} {{$sessionUser->lastname}}</span>
     <a href="/profile"><i class="fa-solid fa-user fa-xl" style="color: #ffffff;"></i> Profile </a>
-    <a href="/logout"><i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i> Logout</a>
+    <a href="/logout"> <i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i>Logout</a>
 @else
-    <a href="/login">Sign in</a>
-    <a href="/register">Register</a>
+    <a href="/login"> <i class="fa-solid fa-user fa-xl" style="color: #ffffff;"></i>Sign in</a>
+    <a href="/register"><i class="fa-solid fa-right-from-bracket" style="color: #ffffff;"></i>Register</a>
 @endif
 
 
